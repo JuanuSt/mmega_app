ce962fc77b3cbe52d9984d65753807959cf98385
juanu <juanu13@gmail.com>
2018-12-16 11:44:59 +0000

    full_install_mmega.sh edited online with Bitbucket

--- full_install_mmega.sh
+++ full_install_mmega.sh
@@ -21,7 +21,7 @@
   sudo apt -y install python-pip
   wget -q https://mega.nz/linux/MEGAsync/xUbuntu_18.04/amd64/megacmd-xUbuntu_18.04_amd64.deb
   sudo apt -y install -f ./megacmd-xUbuntu_18.04_amd64.deb
-  rm megacmd-xUbuntu_16.04_amd64.deb
+  rm megacmd-xUbuntu_16.08_amd64.deb
 
 # Download app
   ssh-keyscan -H bitbucket.org >> ~/.ssh/known_hosts
