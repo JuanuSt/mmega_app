6f6b7360235f11cd5ec731cf5adbccbb288fa7c9
juanust <juanu13@gmail.com>
2018-12-18 23:16:21 +0100

    add right menu and redirect to new tab when clicking in file

--- static/js/mmega.js
+++ static/js/mmega.js
@@ -31,24 +31,3 @@ function upload(id) {
 function sync(id) {
 	window.location.href=('/sync/' + id)
 }
-
-
-/* 
-document.querySelectorAll('input[type=number]')
-.forEach(e => e.oninput = () => {
-  // Always 2 digits
-  if (e.value.length >= 2) e.value = e.value.slice(0, 2);
-  // 0 on the left (doesn't work on FF)
-  if (e.value.length === 1) e.value = '0' + e.value;
-  // Avoiding letters on FF
-  if (!e.value) e.value = '00';
-});
-
-function todaydate(){
-    var today_date= new Date()
-    var myyear=today_date.getYear()
-    var mymonth=today_date.getMonth()+1
-    var mytoday=today_date.getDate()
-    document.write(myyear+"/"+mymonth+"/"+mytoday)
-}
- */
---
+++ static/js/right_menu.js
@@ -0,0 +1,42 @@
+/* Avoiding jQuery */
+function onDOMReady(f){/in/.test(document.readyState)?setTimeout(arguments.callee.name+'('+f+')',9):f()}
+function isNotBatman(a,h){for(;a&&a!==document;a=a.parentNode){if(a.classList.contains(h.substr(1))){return 1}}}
+function fadeElement(a,b){if(b!=='show'){return a.style.opacity=setTimeout(function(){a.style.display='none'},200)*0}a.style.display='block';setTimeout(function(){a.style.opacity=1},30)}
+function addListener(a,b,c){((typeof a=="string")?document.querySelector(a):a).addEventListener(b,c)}
+
+/* Right menu code */
+onDOMReady(function(){
+	Array.from(document.querySelectorAll(".rmenu-host")).forEach((z,i)=>{
+		addListener(z,"contextmenu",function(event){
+			Array.from(document.querySelectorAll(".rmenu")).forEach((k,i)=>{k.style.display='none'});
+			event.preventDefault();
+			var mID='';
+			Array.from(z.classList).forEach((y,i)=>{if(~y.indexOf("rmenu-id-")){mID='.'+y}});
+			x=document.querySelector(".rmenu"+mID);
+			var maxLeft=(window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth)-10-x.getBoundingClientRect().width;
+			var maxTop=(window.innerHeight||document.documentElement.clientHeight||document.body.clientHeight)-10-x.getBoundingClientRect().height;
+			fadeElement(x,'show');
+			x.style.left=(event.pageX>maxLeft?maxLeft:event.pageX)+"px",
+			x.style.top=(event.pageY>maxTop?maxTop:event.pageY)+"px"
+		})
+	});
+	Array.from(document.querySelectorAll(".rmenu li")).forEach((x,i)=>{
+		addListener(x,"click",function(){
+			if(eval("typeof(handleMenuAction)==typeof(Function)")&&!x.classList.contains("disabled")) handleMenuAction(x.getAttribute("data-action"));
+			fadeElement(x.parentElement,'hide')
+		})
+	});
+	addListener(document,"mousedown",function(e){
+		if(!isNotBatman(e.target,".rmenu-host")) Array.from(document.querySelectorAll(".rmenu")).forEach((x,i)=>{fadeElement(x,'hide')})
+	})
+});
+
+/* Right menu actions */
+function handleMenuAction(evt) {
+	alert("Action required: " + evt);
+}
+
+
+
+
+
