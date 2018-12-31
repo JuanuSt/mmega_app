9a16dfbf27bfaccce39518b4a08f08bb7ef4fefe
juanu <juanu13@gmail.com>
2018-12-31 15:28:46 +0000

    install_dependencies.sh edited online with Bitbucket

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,14 +4,20 @@
 # Run this script from mmega_app directory
 # Uncomment the lines if you want to upgrade and install virtualenv
 
-sudo apt update
-#sudo apt -y upgrade
-sudo apt -y install megatools
-wget -q https://mega.nz/linux/MEGAsync/xUbuntu_16.04/amd64/megacmd-xUbuntu_16.04_amd64.deb
-sudo apt -y install -f ./megacmd-xUbuntu_16.04_amd64.deb
-rm megacmd-xUbuntu_16.04_amd64.deb
-sudo apt -y install python-pip
-#sudo apt -y install virtualenv
-pip install -r requeriments.txt
+# Install system packages
+  sudo -S apt update
+  #sudo apt -y upgrade
+  sudo apt -y install ssh
+  sudo apt -y install git
+  sudo apt -y install wget
+  sudo apt -y install nginx
+  sudo apt -y install redis-server
+  sudo apt -y install megatools
+  sudo apt -y install python-pip
+  wget -q https://mega.nz/linux/MEGAsync/xUbuntu_18.04/amd64/megacmd-xUbuntu_18.04_amd64.deb
+  sudo apt -y install -f ./megacmd-xUbuntu_18.04_amd64.deb
+  rm megacmd-xUbuntu_18.04_amd64.deb
+  #sudo apt -y install virtualenv
+  pip install -r requeriments.txt
 
 exit
